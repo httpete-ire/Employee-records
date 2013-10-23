c4d4bb459acf32aad29ae14f11f87e7453dd6849
Pete <redmondp@gmail.com>
2013-10-23 12:49:25 +0100

    Shop.js done

---
+++ assets/js/Shop.js
@@ -0,0 +1,49 @@
+//constructor function
+
+function Shop(id, n, l, t) {
+  this.id = id;
+  this.name = n;
+  this.location = l;
+  this.employees = new Array();
+  this.turnover = t;
+}
+
+Shop.prototype = {
+
+  getId : function() {
+    return this.id;
+  },
+  setId : function(i) {
+    this.id = i;
+  },
+  getName : function() {
+    return this.name;
+  },
+  setName : function(n) {
+    this.name = n;
+  },
+  getLocation : function() {
+    return this.location;
+  },
+  setLocation : function(l) {
+    this.location = l;
+  },
+  addEmployee : function(employee) {
+    this.employees.push(employee);
+  },
+  removeEmployee : function(employee) {
+    //get index of employee in array
+    var index = this.employees.indexOf(employee);
+    //if employee in array, remove from array
+    if(index !== -1) {
+      this.employees.splice(index,1);
+    }
+  },
+  getTurnover : function() {
+    return this.turnover;
+  },
+  setturnover : function(t) {
+    this.turnover = t;
+  }
+}
+
---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>N00053275 - Web Programming CA 1</title>
+</head>
+<body>
+
+  <script type="text/javascript" src="assets/js/Shop.js"></script>
+  <script type="text/javascript" src="assets/js/Employee.js"></script>
+
+</body>
+</html>
